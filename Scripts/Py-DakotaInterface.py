dba3bca9091e2e96c27c9e8bc5660b1c55c9421b
Nam Young Kim <nkim305@gatech.edu>
2019-04-17 14:34:17 -0400

    Latestchanges to interface

--- Scripts/Py-DakotaInterface.py
+++ Scripts/Py-DakotaInterface.py
@@ -13,7 +13,7 @@ import dakota.interfacing as di
 tau_ent_main = 10#*miliseconds
 tau_ent_sec = 1#*miliseconds
 phi_overall = 0.635
-
+flame_lib_loc = "C:\\Users\\Hazelle\\dakota_gui_workspace\\NOxOpt3(Phim,s+Trs)\\FlameLibrary"
 params, results = di.read_parameters_file()
 
 #* Input parameters (design variables) from Dakota Input File
@@ -29,12 +29,14 @@ msec = mfs+mas
 Msratio = msec/(msec+mmain) # ratio of secondary mass to total mass must be 20% or less
 # MdotsRatio =(msec/tau_ent_sec)/((msec/tau_ent_sec)+(mmain/tau_ent_main)) 
 constraint_violated = Msratio >= 0.2 # constraint violated if secondary mass > 20% of total mass #TODO: Change this to depend on Dakota inputs!!
-NOx = 0; Temp = 0; #COs = 0;
+
 
 #running case
 if not constraint_violated:
-    (out_dF) = finite_entrainment(phi_overall, phi_main, tau_res_sec, tau_ent_main, tau_ent_sec, phi_jet=phi_sec,tau_global=20,write_df='False') #input everything in miliseconds
-    temp = out_dF["T_Max"]; NOx = out_dF['NO_ppmvd_constraint']; #COs = out_dF['CO_ppmvd_constraint'], for the weighted optimization
+    (out_dF) = finite_entrainment(phi_overall, phi_main, tau_res_sec, tau_ent_main, tau_ent_sec, phi_jet=phi_sec,tau_global=20,write_df='False',flame_file=os.path.join(flame_lib_loc, f"phi_main_{phi_main:.4f}_GRI30_25atm.pickle")) #input everything in miliseconds
+    temp = out_dF["T_constraint"]; 
+    NOx = out_dF['sys_NO_ppmvd'];
+    COs = out_dF['sys_CO_ppmvd'];
 else:
     temp = 0
     NOx = 1e6
@@ -48,7 +50,7 @@ for i, r in enumerate(results.responses()):
             r.function = temp
         if i == 2:
             r.function = Msratio
-        # if i == 3:
-        #     r.function = MdotsRatio
+        if i == 3:
+            r.function = COs
 #need no gradients, hessians
 results.write()
